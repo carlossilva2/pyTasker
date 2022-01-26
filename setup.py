9e14f15f899a73237fcc34f3a05392d0c66cbb69
Carlos Silva <cmiguelrsilva@gmail.com>
2022-01-26 12:48:01 +0000

    Pypi deploy first version

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     author = "Carlos Silva",
     author_email = "carlos.miguel.silva@protonmail.com",
-    name = "Tasker",
+    name = "pyTasker",
     description = "Run pipelines on your own computer for better automation",
-    version = "0.0.1",
-    packages = [ "Tasker" ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version = "0.1.0",
+    url="https://gitlab.com/carlossilva2/tasker",
+    packages = find_packages(),
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
     entry_points = {
         "console_scripts": [
             "tasker = Tasker.__main__:main"
@@ -14,5 +30,8 @@ setup(
     },
     install_requires = [
 
-    ]
+    ],
+    package_data={
+        "static": ["*"]
+    }
 )
